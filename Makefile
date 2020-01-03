e11dc3cfd44777198c7265eb6a25e4c10fe0e242
nomike <nomike@nomike.com>
2020-01-03 01:13:12 +0100

    Make songbook creation more sophisitcated: * add "create_songbook.py" to take care of computing the command line arguments for the call to "chordpro" * allow files "guitar-songlist" and "ukulele-songist" to contain a sorted list of songfiles which will be put into the songbook first, followed by any additional songs, not included into the songlist files

--- Makefile
+++ Makefile
@@ -3,11 +3,12 @@ ukulele: songbook-ukulele.pdf
 guitar: songbook-guitar.pdf
 cover-ukulele: cover-ukulele.pdf
 cover-guitar: cover-guitar.pdf
+
 songbook-ukulele.pdf : *.chopro chordpro-ukulele.json cover-ukulele.pdf
-	find . -type f -a -name "*.chopro" -a ! -name "*-guitar.chopro" -print0 | sort -z | xargs --null chordpro --config chordpro-ukulele.json --output songbook-ukulele.pdf --cover cover-ukulele.pdf
+	./create_songbook.py --instrument ukulele
 
 songbook-guitar.pdf : *.chopro chordpro-guitar.json cover-guitar.pdf
-	find . -type f -a -name "*.chopro" -a ! -name "*-ukulele.chopro" -print0 | sort -z | xargs --null chordpro --config chordpro-guitar.json --output songbook-guitar.pdf --cover cover-guitar.pdf
+	./create_songbook.py --instrument guitar
 
 clean:
 	rm -f songbook-ukulele.pdf songbook-guitar.pdf cover-guitar.pdf cover-guitar.aux cover-guitar.log cover-ukulele.pdf cover-ukulele.aux cover-ukulele.log
---
+++ create_songbook.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Usage:
+    create_songbook.py --instrument=<instrument>
+"""
+import glob
+import re
+import subprocess
+from docopt import docopt
+
+instruments = ['ukulele', 'guitar']
+
+if __name__ == "__main__":
+    arguments = docopt(__doc__, version='creae_songbook.py v. 0.1')
+    if not arguments["--instrument"] in instruments:
+        raise Exception("Unsupported instrument")
+    target_instrument = arguments["--instrument"]
+    excludeded_instruments = [re.compile('^.*-%s.chopro$' % (i)) for i in instruments if i != target_instrument]
+    with open('%s-songlist' % (target_instrument), 'r') as file:
+        lines = [i.rstrip('\n') for i in file.readlines()]
+
+    filelist = glob.glob('*.chopro')
+    filelist.sort()
+    for file in filelist:
+        for p in excludeded_instruments:
+            if not p.match(file):
+                if not file in lines:
+                    lines.append(file)
+    command = ['/usr/local/bin/chordpro', '--config', 'chordpro-%s.json' % (target_instrument), '--output', 'songbook-%s.pdf' % (target_instrument), '--cover', 'cover-%s.pdf' % (target_instrument)]
+    command.extend(lines)
+    subprocess.run(command)
