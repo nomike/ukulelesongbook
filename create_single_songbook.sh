63fb0ac43041ebc28d7340c252bf75727d182163
nomike <nomike@nomike.com>
2020-08-05 23:09:46 +0200

    Fix relative paths in create_single_songbook.sh

--- create_single_songbook.sh
+++ create_single_songbook.sh
@@ -25,16 +25,16 @@ mkdir -p "build/${instrument}/songs"
 
 # Make sure the cover has an odd number of pages
 if [[ $(( $( pdfinfo "cover/cover-${instrument}.pdf" | grep "Pages:" | awk '{print $2}' ) % 2 )) -eq 0 ]]; then
-    pdfunite "cover/cover-${instrument}.pdf" ../build/empty.pdf "../build/${instrument}/temp.out"
-    mv "../build/${instrument}/temp.out" "build/${instrument}/cover.pdf"
+    pdfunite "cover/cover-${instrument}.pdf" build/empty.pdf "build/${instrument}/temp.out"
+    mv "build/${instrument}/temp.out" "build/${instrument}/cover.pdf"
 else
     cp "cover/cover-${instrument}.pdf" "build/${instrument}/cover.pdf"
 fi
 
 # Make sure the toc has an even number of pages
 if [[ $(( $( pdfinfo "build/${instrument}/toc.pdf" | grep "Pages:" | awk '{print $2}' ) % 2 )) -eq 1 ]]; then
-    pdfunite "build/${instrument}/toc.pdf" ../build/empty.pdf "../build/${instrument}/temp.out"
-    mv "../build/${instrument}/temp.out" "build/${instrument}/toc.pdf"
+    pdfunite "build/${instrument}/toc.pdf" build/empty.pdf "build/${instrument}/temp.out"
+    mv "build/${instrument}/temp.out" "build/${instrument}/toc.pdf"
 fi
 
 pdfunite "build/${instrument}/cover.pdf" "build/${instrument}/toc.pdf" "build/${instrument}/songs/"*.pdf out/songbook-${instrument}-single.pdf
