6faf8b30e06468e11a70f303af630adfd866ebf5
nomike <nomike@nomike.com>
2020-08-31 18:25:22 +0200

    Make code DRY in "update_commit_hashes.sh".

--- update_commit_hashes.sh
+++ update_commit_hashes.sh
@@ -1,13 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ ${#} -eq 1 ] ; then
     GVF="build/COMMIT-HASH-${1}"
-    VN=$(cd "${1}" ; git rev-parse HEAD 2>/dev/null)
+    pushd "${1}" >/dev/null
 else
     GVF="build/COMMIT-HASH"
-    VN=$(git rev-parse HEAD 2>/dev/null)
+    pushd . >/dev/null
 fi
 
+VN=$(git rev-parse HEAD 2>/dev/null) || VN="not a git directory"
+popd >/dev/null
 
 if test -r "${GVF}" ; then
 	VC=$(cat ${GVF})
