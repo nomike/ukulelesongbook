747347ab66c8db99eb61f4de7e795d20cf9db82d
nomike <nomike@nomike.com>
2020-10-23 01:13:20 +0200

    Return error code if checksongs detects an issue.

--- checksongs.py
+++ checksongs.py
@@ -3,6 +3,7 @@
 import glob
 import os
 import re
+import sys
 
 def rchop(s, suffix):
     if suffix and s.endswith(suffix):
@@ -13,13 +14,19 @@ if __name__ == "__main__":
 
     for (s, a) in [f[:-7].split(" - ") for f in glob.glob('songs/*.chopro')]:
         filename = '%s - %s.chopro' % (s, a)
+        issue = False
         with open(filename, 'r') as file:
             content = file.read()
             song = os.path.basename(s)
             artist = rchop(rchop(a, '-ukulele'), '-guitar')
             
             if not re.search("{t(?:itle){0,1}: {0,1}%s}" % song.replace("(", "\\(").replace(")", "\\)"), content.replace('/', '_')):
+                issue = True
                 print("Wrong title in song '%s'" % (filename))
             if not re.search("{artist: {0,1}%s}" % artist.replace("(", "\\(").replace(")", "\\)"), content.replace('/', '_')):
+                issue = True
                 print("Wrong artist in song '%s'" % (filename))
-            
+    if issue:
+        sys.exit(1)
+    else:
+        sys.exit(0)
