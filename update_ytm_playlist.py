0c88ac02ca9f492f96ac2b6e0a575a2be40a0b74
nomike <nomike@nomike.com>
2021-05-05 11:44:04 +0200

    Add a caching mechanism to update_ytm_playlist which also allows for correcting mistakes.

--- update_ytm_playlist.py
+++ update_ytm_playlist.py
@@ -2,13 +2,14 @@
 
 from ytmusicapi import YTMusic
 from glob import glob
+import os
+import json
 
 def get_song(title):
     songs = ytmusic.search(query=title, filter="songs", limit=1)
     if len(songs) == 0:
         raise Exception("Could not find song {}".format(title))
     return songs[0]
-
 
 if __name__ == "__main__":
     ytmusic = YTMusic('config/headers_auth.json')
@@ -19,7 +20,18 @@ if __name__ == "__main__":
     else:
         playlist = playlists[0]
 
-    for songtitle in [filename.split('.chopro')[0] for filename in glob('songs/*.chopro')]:
-        song = get_song(songtitle)
-        ytmusic.add_playlist_items(playlist['playlistId'], [song['videoId']])
-
+    for songtitle in [os.path.basename(filename).split('.chopro')[0] for filename in glob('songs/*.chopro')]:
+        try:
+            if os.path.exists(f'songs/{songtitle}.json'):
+                with open(f'songs/{songtitle}.json', 'r') as song_info_file:
+                    song_info = json.load(song_info_file)
+            else:
+                song_info = {}
+                song = get_song(songtitle)
+                song_info['YouTubeVideoId'] = song['videoId']
+                with open(f'songs/{songtitle}.json', 'w') as song_info_file:
+                    json.dump(song_info, song_info_file)
+            
+            ytmusic.add_playlist_items(playlist['playlistId'], [song_info['YouTubeVideoId']])
+        except Exception:
+            pass
