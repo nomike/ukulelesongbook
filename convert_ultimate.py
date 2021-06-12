0d86c57cb23d78fabea890c80d0d05c886c847d9
nomike <nomike@nomike.com>
2021-06-12 14:18:23 +0200

    Improve convert_ultimate to handle "x4" comments and strip trailing whitespace.

--- convert_ultimate.py
+++ convert_ultimate.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
     last_section = None
     re_section_header = re.compile('^\[([^\]]*)\]$')
     re_numbered = re.compile('^(.*) [0-9]*$')
+    re_repeat = re.compile('^(.*)(x[0-9]{1,2})$')
     with open(sys.argv[1], 'r') as file:
         lines = [i.rstrip('\n') for i in file.readlines()]
     (song, artist) = os.path.basename(sys.argv[1])[:-4].split(" - ")
@@ -26,13 +27,14 @@ if __name__ == "__main__":
     unskip_empty_line = True
     was_empty_line = False
     for i in range(0, len(lines)):
-        line = lines[i]
+        line = lines[i].rstrip()
         match = re_section_header.match(line)
         if was_empty_line and unskip_empty_line and not match:
             unskip_empty_line = False
             print("")
 
         if len(line) == 0:
+            # this line is empty
             was_empty_line = True
             continue
         else:
@@ -40,6 +42,7 @@ if __name__ == "__main__":
             unskip_empty_line = True
 
         if match:
+            # this line is a section header
             unskip_empty_line = False
             if last_section:
                 print('{end_of_%s}\n' % (last_section.replace('-', '').replace(' ', '_')))
@@ -52,8 +55,14 @@ if __name__ == "__main__":
             if last_section == "verse":
                 verse_count = verse_count + 1
         else:
-            if not (len(line) == 0 and i < (len(lines) - 1) and lines[i+1].startswith("[")):
-                print(line)
+            # this line is a normal line
+            match = re_repeat.match(line)
+            if match:
+                print(match[1].rstrip())
+                print(f'{{c: {match[2]}}}')
+            else:
+                if not (len(line) == 0 and i < (len(lines) - 1) and lines[i+1].startswith("[")):
+                    print(line)
     if last_section:
         print('{end_of_%s}' % (last_section))
 
